d8ee5b77a298e3bc15084caffdfe8ee827de534d
Amulya singabhattu <amulyasingbhattu11@gmail.com>
2026-07-03 18:23:32 +0530

    toggle functionality

--- frontend/index.js
+++ frontend/index.js
@@ -1,2 +1,26 @@
+const users=[
+    {
+        "name": "Jane Doe",
+        "gender": "Female",
+        "image": "../Images/jane.png"
+    },
+    {
+    "name" :"John Doe",
+    "gender" : "Male",
+    "image" : "../Images/john.png"
+  },
+];
+let curUserId=0;
+function toggle(){
+    if(curUserId==0){
+        curUserId=1;
+    }
+    else{
+        curUserId=0;
+    }
+    document.getElementById("img").src=users[curUserId].image;
+     document.getElementById("card-name").innerText=users[curUserId].name;
+     document.getElementById("card-gender").innerText=users[curUserId].gender;
+}
 console.log("Hello from JS");
 //alert("Hello from JS");
