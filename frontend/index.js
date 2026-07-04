f519d64e17bc1cb8cbf6644056282b825a254228
Amulya singabhattu <amulyasingbhattu11@gmail.com>
2026-07-04 10:01:03 +0530

    added random users through fetching api

--- frontend/index.js
+++ frontend/index.js
@@ -21,6 +21,30 @@ function toggle(){
     document.getElementById("img").src=users[curUserId].image;
      document.getElementById("card-name").innerText=users[curUserId].name;
      document.getElementById("card-gender").innerText=users[curUserId].gender;
-}
+    }
+    function getRandomUser()
+    {
+      fetch("https://randomuser.me/api")
+        .then(function (data){
+          return data.json()
+        })
+        .then(function (parsedData){
+          // console.log(parsedData)
+          let gender = parsedData.results[0].gender;
+    
+          let first = parsedData.results[0].name.first;
+          let last = parsedData.results[0].name.last;
+          let name = first  + " " + last;
+          let imageUrl = parsedData.results[0].picture.large;
+    
+          // gender
+          document.getElementById("card-gender").innerText=gender;
+          // name
+          document.getElementById("card-name").innerText=name
+          // img
+          document.getElementById("img").src=imageUrl
+    
+        })
+    }
 console.log("Hello from JS");
 //alert("Hello from JS");
